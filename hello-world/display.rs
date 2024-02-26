bb959e636e6d8c145ec2e075c9a890bd28e59c17
Juan Pedro Molina <otojupe@gmail.com>
2024-02-26 11:27:11 +0100

    display formatter activity

--- hello-world/display.rs
+++ hello-world/display.rs
@@ -41,6 +41,28 @@ impl fmt::Display for Complex {
     }
 }
 
+// TestCase List
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{0}: {1}", count, v)?;
+        }
+
+
+        write!(f, "]")
+    }
+}
+
+
+
 fn main() {
 
     println!("Structure {}", Structure(23));
@@ -60,4 +82,6 @@ fn main() {
     println!("Display {}", complex);
     println!("Debug {:?}", complex);
 
+    let v = List(vec![1,2,3]);
+    println!("{}", v);
 }
